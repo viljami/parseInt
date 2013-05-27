49399f76e97d335ac96ca7173270a063cbf579f8
Viljami Peltola <webengineer@viljamipeltola.com>
2013-05-27 11:50:04 +0300

    Comments added.

--- c/main2.c
+++ c/main2.c
@@ -8,14 +8,23 @@ void main(int argc, char** argv)
     }
 }
 
+/**
+ * Function parses String to Int. Parsing stops if value outside 0 - 9 is met.
+ *
+ * Param  String to be parsed.
+ * Return Parsed Integer.
+ */
 int get_integer_from_string( const char* str ) {
 	int value = 0, power = 1, tmp = 0, size = 0, sign = 1;
 	const char* c = NULL;
+
+	// Is the number positive or negative
 	if( str[0] == '-') {
 		sign = -1;
 		str++;
 	}
 
+	// Validation
 	for(c = str; *c != '\0'; c++){
 		tmp = *c - '0';
 		if( tmp < 0 || tmp > 9 ) break;
@@ -23,11 +32,12 @@ int get_integer_from_string( const char* str ) {
 		size++;
 	}
 
+	// Parsing
 	for(c = str; *c != '\0'; c++){
 		power /= 10;
 		value += (*c - '0') * power;
 		if( power <= 1 ) break;
 	}
-	
+
 	return value * sign;
 }
